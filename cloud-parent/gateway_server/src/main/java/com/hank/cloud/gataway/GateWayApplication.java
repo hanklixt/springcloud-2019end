ea9f72a16cd49657cbb8a86bf5318a670baf49b9
hankli <1033110657@qq.com>
2019-12-04 17:26:16 +0800

    抽取spring-starter-web在父工程坐标，移动到对应子工程，spring-gateway使用webflux,和springmvc有冲突

---
+++ cloud-parent/gateway_server/src/main/java/com/hank/cloud/gataway/GateWayApplication.java
@@ -0,0 +1,17 @@
+package com.hank.cloud.gataway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Description
+ * @auther 李先涛
+ * @create 2019-12-04 17:08
+ */
+@SpringBootApplication
+public class GateWayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GateWayApplication.class,args);
+    }
+
+}
