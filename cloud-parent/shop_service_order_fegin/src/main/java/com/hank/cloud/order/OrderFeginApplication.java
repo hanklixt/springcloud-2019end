b178ede85f2abdd012b1b978e046e3f599944cbd
hankli <1033110657@qq.com>
2019-12-04 13:09:16 +0800

    hystrix monitor配置

--- cloud-parent/shop_service_order_fegin/src/main/java/com/hank/cloud/order/OrderFeginApplication.java
+++ cloud-parent/shop_service_order_fegin/src/main/java/com/hank/cloud/order/OrderFeginApplication.java
@@ -4,13 +4,16 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableFeignClients
-public class OrderFeginApplication {
+@EnableCircuitBreaker   //无论是feign,还是ribbon+restTemplate 要想获取熔断数据，都需要打开此注解
+@EnableHystrixDashboard   //打开web界面，产看熔断信息
+ public class OrderFeginApplication  {
 
 	public static void main(String[] args) {
 		SpringApplication.run(OrderFeginApplication.class,args);
